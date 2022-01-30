5b6434c7ee0278c8baf2cab6d7cad8cab9d3cd48
Chany Rizqina <40732047+mindty@users.noreply.github.com>
2022-01-30 08:23:28 +0700

    server

---
+++ server/index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mysql = require("mysql");
+
+app.use(cors());
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "todos"
+});
+
+// ---------------- GET ALL TODO ------------------//
+app.get("/api/getAllToDo", (req, res) => {
+  
+  const sqlSelect ="SELECT * FROM todo;";
+  db.query(sqlSelect, (err, result) => {
+    console.log(result);
+    console.log(err);
+
+    res.send(result);
+    if(err){
+      res.send(err);
+    }
+  })
+})
+
+// ---------------- POST NEW TODO ------------------//
+app.post("/api/addToDo", (req, res) => {
+  
+  const name = req.body.name
+  const status = req.body.status
+  
+  const sqlInsert ="INSERT INTO todo(name, status) VALUES (?, ?);";
+  db.query(sqlInsert, [name, status], (err, result) => {
+    console.log(result)
+    console.log(err)
+
+    if(err){
+      res.send(err);
+    }
+  })
+})
+
+app.listen(3001, () =>{
+  console.log("running on port 3001");
+});
